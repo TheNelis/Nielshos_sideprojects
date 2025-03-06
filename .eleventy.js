442f19d65e9caa2377349fb1dd6495bc4ab55fc3
Niels Hos <nielshos01@gmail.com>
2025-03-06 13:09:14 +0100

    first-commit

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/three/build/": "js/three/"
+  });
+
+  eleventyConfig.addWatchTarget("src/css/");
+  
+    return {
+        dir: {
+          input: 'src',
+          includes: '_includes',
+          output: '_site',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+      };
+};
