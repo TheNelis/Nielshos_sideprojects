ba2cbbdbc1abeb7d9c5b7b5b7ab379cbf2ad6d4b
Niels Hos <nielshos01@gmail.com>
2025-03-09 16:19:30 +0100

    grid-test

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/css/");
+
+  // Three.js
   eleventyConfig.addPassthroughCopy({
     "node_modules/three/build/": "js/three/"
   });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/three/examples/jsm/renderers/CSS2DRenderer.js": "js/CSS2DRenderer.js"
+  });
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/three/examples/jsm/controls/OrbitControls.js": "js/OrbitControls.js"
+  });
+  
 
   eleventyConfig.addWatchTarget("src/css/");
   
